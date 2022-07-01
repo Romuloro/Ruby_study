00733d07a7cc6b2751387b830f601afed195e038
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-07-01 12:09:41 -0300

    conta bancária change

--- POO_ruby/ContaBancaria/class/conta_bancaria_com_taxa.rb
+++ POO_ruby/ContaBancaria/class/conta_bancaria_com_taxa.rb
@@ -2,7 +2,7 @@ class ContaBancariaComTaxa < ContaBancaria
     def transferir(outra_conta, valor)
         taxa = 2
         if saldo > valor
-            debitar(taxa,)
+            debitar(taxa)
             super
         end
     end
